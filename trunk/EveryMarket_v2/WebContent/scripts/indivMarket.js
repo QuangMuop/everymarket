799f67efd89a844a8bfcfff8dd5fda7ecee6ee37
kitchu@naver.com <kitchu@naver.com@762cc4ab-73c6-631b-9375-4613270a7d42>
2013-04-28 04:43:36 +0000

    시험용 커밋. 다들 커밋로그 잘 써주세여.

--- trunk/EveryMarket_v2/WebContent/scripts/indivMarket.js
+++ trunk/EveryMarket_v2/WebContent/scripts/indivMarket.js
@@ -89,7 +89,7 @@ $(document).ready(function(){
 						"<option value='" + category.ct_id + "'>" + 
 						category.ct_large + " : " + category.ct_small + "</option>");
 				});
-			}			
+			}		
 		);
 		
 		$("#registerProduct").bPopup();
