33b0ee6872ce77d5ed33a8b659eafb4eb49fcd29
kpj5427 <kpj5427@762cc4ab-73c6-631b-9375-4613270a7d42>
2013-04-30 01:57:04 +0000

    메일 센더 커미트

---
+++ EveryMarket_v2.1/src/everymarket/mailsender/AbstractMailSender.java
@@ -0,0 +1,16 @@
+package everymarket.mailsender;
+
+import javax.mail.MessagingException;
+
+import org.springframework.mail.javamail.JavaMailSender;
+
+public abstract class AbstractMailSender {
+	protected JavaMailSender sender;
+
+	public void setSender(JavaMailSender sender) {
+		this.sender = sender;
+	}
+	
+	public abstract void sendMail (String to, String from, String subject, String text) throws MessagingException;
+	
+}
