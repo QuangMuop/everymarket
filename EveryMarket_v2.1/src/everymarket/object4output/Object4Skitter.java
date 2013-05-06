3499386dae2115148ae76b871ced3d7f086ee5a5
kitchu <kitchu@naver.com>
2013-05-06 09:13:55 +0000

    자리 옮기기전에 마지막.

---
+++ EveryMarket_v2.1/src/everymarket/object4output/Object4Skitter.java
@@ -0,0 +1,93 @@
+package everymarket.object4output;
+
+import everymarket.model.Blog;
+import everymarket.model.Product;
+import everymarket.model.Review;
+
+public class Object4Skitter {
+	private String owner_m_id;
+	private String owner_m_nick;
+	private String owner_b_main;
+	private Product recentProduct_1;
+	private Product recentProduct_2;
+	private Product recentProduct_3;
+	private Blog randomDangol_1;
+	private Blog randomDangol_2;
+	private Blog randomDangol_3;
+	private Review recentReview_1;
+	private Review recentReview_2;
+	private Review recentReview_3;
+	
+	public String getOwner_m_id() {
+		return owner_m_id;
+	}
+	public void setOwner_m_id(String owner_m_id) {
+		this.owner_m_id = owner_m_id;
+	}
+	public String getOwner_m_nick() {
+		return owner_m_nick;
+	}
+	public void setOwner_m_nick(String owner_m_nick) {
+		this.owner_m_nick = owner_m_nick;
+	}
+	public String getOwner_b_main() {
+		return owner_b_main;
+	}
+	public void setOwner_b_main(String owner_b_main) {
+		this.owner_b_main = owner_b_main;
+	}
+	public Product getRecentProduct_1() {
+		return recentProduct_1;
+	}
+	public void setRecentProduct_1(Product recentProduct_1) {
+		this.recentProduct_1 = recentProduct_1;
+	}
+	public Product getRecentProduct_2() {
+		return recentProduct_2;
+	}
+	public void setRecentProduct_2(Product recentProduct_2) {
+		this.recentProduct_2 = recentProduct_2;
+	}
+	public Product getRecentProduct_3() {
+		return recentProduct_3;
+	}
+	public void setRecentProduct_3(Product recentProduct_3) {
+		this.recentProduct_3 = recentProduct_3;
+	}
+	public Blog getRandomDangol_1() {
+		return randomDangol_1;
+	}
+	public void setRandomDangol_1(Blog randomDangol_1) {
+		this.randomDangol_1 = randomDangol_1;
+	}
+	public Blog getRandomDangol_2() {
+		return randomDangol_2;
+	}
+	public void setRandomDangol_2(Blog randomDangol_2) {
+		this.randomDangol_2 = randomDangol_2;
+	}
+	public Blog getRandomDangol_3() {
+		return randomDangol_3;
+	}
+	public void setRandomDangol_3(Blog randomDangol_3) {
+		this.randomDangol_3 = randomDangol_3;
+	}
+	public Review getRecentReview_1() {
+		return recentReview_1;
+	}
+	public void setRecentReview_1(Review recentReview_1) {
+		this.recentReview_1 = recentReview_1;
+	}
+	public Review getRecentReview_2() {
+		return recentReview_2;
+	}
+	public void setRecentReview_2(Review recentReview_2) {
+		this.recentReview_2 = recentReview_2;
+	}
+	public Review getRecentReview_3() {
+		return recentReview_3;
+	}
+	public void setRecentReview_3(Review recentReview_3) {
+		this.recentReview_3 = recentReview_3;
+	}
+}
