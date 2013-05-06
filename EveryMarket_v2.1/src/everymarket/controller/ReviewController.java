b45729bf2a41b51d228c8ac1ba1382bdbab82d95
kitchu <kitchu@naver.com>
2013-05-06 02:29:01 +0000

    Review테이블 관련 컨트롤러, dao, 쿼리문 생성 및 bean.xml 수정

---
+++ EveryMarket_v2.1/src/everymarket/controller/ReviewController.java
@@ -0,0 +1,13 @@
+package everymarket.controller;
+
+import everymarket.dao.ReviewDao;
+
+public class ReviewController {
+	private ReviewDao daoR;
+
+	public void setDaoR(ReviewDao daoR) {
+		this.daoR = daoR;
+	}
+	
+	
+}
