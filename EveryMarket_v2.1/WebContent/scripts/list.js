b3947df0009453a2da24e75ebf8e9ff3aee3ff1c
kitchu <kitchu@naver.com>
2013-05-06 00:52:19 +0000

    다시 수정:영준

--- EveryMarket_v2.1/WebContent/scripts/list.js
+++ EveryMarket_v2.1/WebContent/scripts/list.js
@@ -1,25 +1,26 @@
-$(document).ready(function() {
-	//이건 이번프로젝트 dwr을 쓴 scoll처리 부분입니다!!
-	$('#item_container').BlocksIt({
-		numOfCol : 5,
-		offsetX : 8,
-		offsetY : 8
-	});	
-	
-		 var category1 = $("#item_container").attr('category');
-		 var searchtext = $("#item_container").attr('searchtext');
-		 
-	
-		 if(category1 == null){			 
-			category1 = "All";
-			
-		 }else{
-			
-		 }
-		
-         var pageNum = 10;
-         
-         $(window).scroll(function(){
+$(document).ready(
+		function() {
+			// 이건 이번프로젝트 dwr을 쓴 scoll처리 부분입니다!!
+			$('#item_container').BlocksIt({
+				numOfCol : 5,
+				offsetX : 8,
+				offsetY : 8
+			});
+
+			var category1 = $("#item_container").attr('category');
+			var searchtext = $("#item_container").attr('searchtext');
+
+			if (category1 == null) {
+				category1 = "All";
+
+			} else {
+
+			}
+
+			var pageNum = 10;
+
+			$(window).scroll(
+					function() {
 
 						$('#item_container').BlocksIt({
 							numOfCol : 5,
@@ -27,42 +28,49 @@ $(document).ready(function() {
 							offsetY : 8
 						});
 
-						if ($(window).scrollTop() == $(document)
-								.height()
+						if ($(window).scrollTop() == $(document).height()
 								- $(window).height()) {
 
-							listDwr.getList(category1, pageNum, searchtext,showList);
+							listDwr.getList(category1, pageNum, searchtext,
+									showList);
 							pageNum += 5;
-							
-							
-						$('#item_container').BlocksIt({
+
+							$('#item_container').BlocksIt({
 								numOfCol : 5,
 								offsetX : 8,
 								offsetY : 8
 
 							});
-						};
-					});     
-         
-		
-        
-		function showList(data) {
-			      
-			$.each(data, function(index, product) {
-         
-               var html = "<div class='grid' id = '" + product.p_id + "'>" ;
-               html += "<div class = 'imgholder'>";
-               html += "<img src='image_product/" + product.p_img + "'/>";
-               html += "</div>";
-               html += "<div>" + product.p_price + "</div>";
-               html += "</div>";
-               $("#item_container").append(html);
-			});
-			
-		
-		}; 
+						}
+						;
+					});
 
-	});
+			function showList(data) {
+
+				$.each(data,
+						function(index, product) {
+
+							var html = "<div class='grid' id = '"
+									+ product.p_id + "'>";
+							html += "<div class = 'imgholder'>";
+							html += "<img src='image_product/" + product.p_img
+									+ "'/>";
+							html += "</div>";
+							html += "<div>" + product.p_price + "</div>";
+							html += "</div>";
+							$("#item_container").append(html);
+						});
+
+				$('#item_container').BlocksIt({
+					numOfCol : 5,
+					offsetX : 8,
+					offsetY : 8
+				});
+
+			}
+			;
+
+		});
 
 var currentWidth = 1100;
 
@@ -93,4 +101,3 @@ $(window).resize(function() {
 		});
 	}
 });
-
