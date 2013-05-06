543516889c8424aed75111bd1ab1cdb626f1de5d
kitchu@naver.com <kitchu@naver.com@762cc4ab-73c6-631b-9375-4613270a7d42>
2013-05-06 08:48:22 +0000

    board_report 오류 쉉

--- trunk/EveryMarket_v2.1/WebContent/scripts/slide_market.js
+++ trunk/EveryMarket_v2.1/WebContent/scripts/slide_market.js
@@ -1,21 +1,50 @@
-$(document).ready(function() {	
-		var options = {};
-		if (document.location.search) {
-			var array = document.location.search.split('=');
-			var param = array[0].replace('?', '');
-			var value = array[1];
+$(document).ready(function() {
+	/*상수 구현부*/
+	var contextUrl = "/EveryMarket_v2.1/"; 
+	
+	$.getJSON(
+		contextUrl + "getDataForSkitter.do", 
+//			+ $("#indivMarketWrapper").attr("owner_id"),
+		function(data){
 			
-			if (param == 'animation') {
-				options.animation = value;
-			}
-			else if (param == 'type_navigation') {
-				options[value] = true;
-				if (value == 'dots') $('.border_box').css({'marginBottom': '40px'});
-			}
 		}
-		$('.box_skitter_large').skitter(options);
+	);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	var options = {};
+	if (document.location.search) {
+		var array = document.location.search.split('=');
+		var param = array[0].replace('?', '');
+		var value = array[1];
 		
-		// Highlight
-		$('pre.code').highlight({source:1, zebra:1, indent:'space', list:'ol'});
-		
-	});
+		if (param == 'animation') {
+			options.animation = value;
+		}
+		else if (param == 'type_navigation') {
+			options[value] = true;
+			if (value == 'dots') $('.border_box').css({'marginBottom': '40px'});
+		}
+	}
+	$('.box_skitter_large').skitter(options);
+	
+	// Highlight
+	$('pre.code').highlight({source:1, zebra:1, indent:'space', list:'ol'});
+	
+});
