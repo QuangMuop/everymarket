af732c4240a08cfb7c02832228567064fc8a89a1
hanssu128 <hanssu128@762cc4ab-73c6-631b-9375-4613270a7d42>
2013-05-08 01:58:55 +0000

    구매요청 승인

--- EveryMarket_v2.1/WebContent/scripts/message.js
+++ EveryMarket_v2.1/WebContent/scripts/message.js
@@ -5,6 +5,11 @@ $(document).ready(function(){
 		location.href="sellrefuse.do?p_id="+p_id;		
 	});
 	
+	$("#bt_sellapprove").click(function(){
+		var p_id = $(this).attr("p_id");
+		location.href="sellapprove.do?p_id="+p_id;
+	});
+	
 });
 
 
