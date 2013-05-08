1ff8367ea47ee43d233dd0213ff9daf9a46572f7
kitchu@naver.com <kitchu@naver.com@762cc4ab-73c6-631b-9375-4613270a7d42>
2013-05-08 07:28:11 +0000

    세션체크 메서드 변경.
    기존 메서드에서 주어진 viewName이 있다면 그 viewName을 가져가게 변경되었음.

--- trunk/EveryMarket_v2.1/src/everymarket/aop/sessionCheck.java
+++ trunk/EveryMarket_v2.1/src/everymarket/aop/sessionCheck.java
@@ -23,14 +23,14 @@ public class sessionCheck {
 		HttpSession session = request.getSession();
 		Object member = session.getAttribute("member");
 		
-		
 		if(member != null){
 			System.out.println("技记 眉农");
 			mav.addObject("member", (Member)member);
-
 		}else{
 			System.out.println("技记 固眉农");
-			mav.setViewName("main_template");
+			if(mav.getViewName() == null){
+				mav.setViewName("main_template");
+			}
 		}
 	}
 }
