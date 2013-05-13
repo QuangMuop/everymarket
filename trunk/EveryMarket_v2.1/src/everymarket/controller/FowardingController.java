fa91cc8f21e03749ea98a41708098402d06e9745
hanssu128 <hanssu128@762cc4ab-73c6-631b-9375-4613270a7d42>
2013-05-13 07:01:16 +0000

    아이디찾기

--- trunk/EveryMarket_v2.1/src/everymarket/controller/FowardingController.java
+++ trunk/EveryMarket_v2.1/src/everymarket/controller/FowardingController.java
@@ -1,6 +1,7 @@
 package everymarket.controller;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -8,6 +9,8 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import everymarket.dao.BlogDao;
@@ -146,4 +149,5 @@ public class FowardingController {
 
 		return listSkitter;
 	}
+	
 }
