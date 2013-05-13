3601f51ba3836c1bd0728c03d59d0fc9651266c4
kitchu <kitchu@naver.com>
2013-05-13 05:14:43 +0000

    세션만료시 경고창 출력과 함께 메인페이지로 가도록 수정.

--- EveryMarket_v2.1/src/everymarket/dwr/HeaderDwr.java
+++ EveryMarket_v2.1/src/everymarket/dwr/HeaderDwr.java
@@ -16,7 +16,11 @@ public class HeaderDwr {
 	public int getCount_unCheckedMessage(HttpServletRequest request){
 		HttpSession session = request.getSession();
 		Member member = (Member)session.getAttribute("member");
-		
-		return daoMsg.getCount_unCheckedMessage(member.getM_id());
+
+		if(member == null){
+			return -1;
+		}else{
+			return daoMsg.getCount_unCheckedMessage(member.getM_id());
+		}
 	}
 }
