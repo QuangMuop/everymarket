c85b284f39676f040da8e1e7f1f2c98ff3fedf85
kitchu@naver.com <kitchu@naver.com@762cc4ab-73c6-631b-9375-4613270a7d42>
2013-05-14 01:06:02 +0000

    단골등록취소버튼 추가

--- trunk/EveryMarket_v2.1/src/everymarket/dao/DangolDao.java
+++ trunk/EveryMarket_v2.1/src/everymarket/dao/DangolDao.java
@@ -4,11 +4,23 @@ import java.util.List;
 
 import org.springframework.orm.ibatis.SqlMapClientTemplate;
 
+import everymarket.model.Dangol;
+
 public class DangolDao {
 	private SqlMapClientTemplate ibatisTemplate;
 
 	public void setIbatisTemplate(SqlMapClientTemplate ibatisTemplate) {
 		this.ibatisTemplate = ibatisTemplate;
+	}
+	
+	/*Output: max(d_id)*/
+	public int getMaxD_id(){
+		return (Integer)ibatisTemplate.queryForObject("getMaxD_id");
+	}
+	
+	/*Input: Dangol / Output: Dangol*/
+	public Dangol checkDangol(Dangol dangol){
+		return (Dangol)ibatisTemplate.queryForObject("checkDangol", dangol);
 	}
 	
 	/*Input: m_id / Output: List<Dangol>*/
@@ -19,5 +31,15 @@ public class DangolDao {
 	/*Input: m_id / Output: count(Dangol)*/
 	public int countDangolByM_id(String m_id){ 
 		return (Integer)ibatisTemplate.queryForObject("countDangolByM_id", m_id);
+	}
+	
+	/*Input: Dangol*/
+	public void registerDangol(Dangol dangol){
+		ibatisTemplate.insert("registerDangol", dangol);
+	}
+	
+	/*Input: Dangol*/
+	public void deleteDangol(Dangol dangol){
+		ibatisTemplate.delete("deleteDangol", dangol);
 	}
 }
