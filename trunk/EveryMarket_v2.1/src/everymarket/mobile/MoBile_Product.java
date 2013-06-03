c152f05c4c0736c243353a4f44c7946cb4fc4216
kitchu@naver.com <kitchu@naver.com@762cc4ab-73c6-631b-9375-4613270a7d42>
2013-06-03 07:37:26 +0000

    responseJson 예제

--- trunk/EveryMarket_v2.1/src/everymarket/mobile/MoBile_Product.java
+++ trunk/EveryMarket_v2.1/src/everymarket/mobile/MoBile_Product.java
@@ -1,8 +1,16 @@
 package everymarket.mobile;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+
+import net.sf.json.JSONObject;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import everymarket.dao.ProductDao;
+import everymarket.model.Product;
 
 @Controller
 public class MoBile_Product {
@@ -12,6 +20,21 @@ public class MoBile_Product {
 		this.daoP = daoP;
 	}
 	
-	
-	
+	/*@RequestMapping("/m_getProductList.do")
+	public void testAndroidd(HttpServletResponse response){
+		System.out.println("[system] ¿äÃ»µé¾î¿È");
+		
+		List<Product> listProduct = daoP.testAndroid("aa");
+				
+		JSONObject jsonObject = new JSONObject();
+		jsonObject.put("listProduct", listProduct);
+		
+		try{
+			response.setContentType("application/json");
+			response.setCharacterEncoding("EUC-KR");
+			response.getWriter().write(jsonObject.toString());
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}*/ 	
 }
