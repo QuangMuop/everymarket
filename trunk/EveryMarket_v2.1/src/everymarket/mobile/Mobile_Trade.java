839418646fb429197c17a1e47a51433859473e20
kitchu@naver.com <kitchu@naver.com@762cc4ab-73c6-631b-9375-4613270a7d42>
2013-06-04 00:36:27 +0000

    커어밋

--- trunk/EveryMarket_v2.1/src/everymarket/mobile/Mobile_Trade.java
+++ trunk/EveryMarket_v2.1/src/everymarket/mobile/Mobile_Trade.java
@@ -1,8 +1,18 @@
 package everymarket.mobile;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+
+import net.sf.json.JSONObject;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import everymarket.dao.TradeDao;
+import everymarket.model.Product;
+import everymarket.model.Trade;
 
 @Controller
 public class Mobile_Trade {
@@ -12,5 +22,43 @@ public class Mobile_Trade {
 		this.daoT = daoT;
 	}
 	
+	@RequestMapping("/m_getBuyList.do")
+	public void m_getBuyList(
+			HttpServletResponse response,
+			@RequestParam("m_id") String m_id){
+		System.out.println("[system] ¿äÃ»µé¾î¿È");
+		
+		List<Trade> listBuy = daoT.getBuyingTrade(m_id);
+				
+		JSONObject jsonObject = new JSONObject();
+		jsonObject.put("listTrade", listBuy);
+		
+		try{
+			response.setContentType("application/json");
+			response.setCharacterEncoding("EUC-KR");
+			response.getWriter().write(jsonObject.toString());
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
 	
+	@RequestMapping("/m_getSellList.do")
+	public void m_getSellList(
+			HttpServletResponse response,
+			@RequestParam("m_id") String m_id){
+		System.out.println("[system] ¿äÃ»µé¾î¿È");
+		
+		List<Trade> listSell = daoT.getSellingTrade(m_id);
+		
+		JSONObject jsonObject = new JSONObject();
+		jsonObject.put("listTrade", listSell);
+		
+		try{
+			response.setContentType("application/json");
+			response.setCharacterEncoding("EUC-KR");
+			response.getWriter().write(jsonObject.toString());
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
 }
