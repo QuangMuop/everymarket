51982ee72ceadc5f6cc7d342a5621738f5b8db99
kitchu@naver.com <kitchu@naver.com@762cc4ab-73c6-631b-9375-4613270a7d42>
2013-06-04 02:40:15 +0000

    dafdsf

--- trunk/EveryMarket_v2.1/src/everymarket/mobile/Mobile_Jjim.java
+++ trunk/EveryMarket_v2.1/src/everymarket/mobile/Mobile_Jjim.java
@@ -1,8 +1,17 @@
 package everymarket.mobile;
 
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+
+import net.sf.json.JSONObject;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import everymarket.dao.JjimDao;
+import everymarket.model.Product;
+import everymarket.object4output.JjimResult;
 
 @Controller
 public class Mobile_Jjim {
@@ -12,6 +21,22 @@ public class Mobile_Jjim {
 		this.daoJ = daoJ;
 	}
 	
-	
+	@RequestMapping("/m_getJjimList.do")
+	public void m_getJjimList(HttpServletResponse response){
+		System.out.println("[system] Âò¸ñ·Ï ¿äÃ»µé¾î¿È");
+		
+		List<Product> listJjim = daoJ.getjjimList("aa");
+				
+		JSONObject jsonObject = new JSONObject();
+		jsonObject.put("listJjim", listJjim);
+		
+		try{
+			response.setContentType("application/json");
+			response.setCharacterEncoding("EUC-KR");
+			response.getWriter().write(jsonObject.toString());
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+	}
 	
 }
