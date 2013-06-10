65822cb9f6320b97eb616bf370d1771ba8437099
skstmdgur@gmail.com <skstmdgur@gmail.com@762cc4ab-73c6-631b-9375-4613270a7d42>
2013-06-10 00:39:05 +0000

    login

--- trunk/EveryMarket_v2.1/src/everymarket/mobile/Mobile_Member.java
+++ trunk/EveryMarket_v2.1/src/everymarket/mobile/Mobile_Member.java
@@ -1,8 +1,15 @@
 package everymarket.mobile;
 
+import javax.servlet.http.HttpServletResponse;
+
+import net.sf.json.JSONObject;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import everymarket.dao.MemberDao;
+import everymarket.model.Member;
 
 @Controller
 public class Mobile_Member {
@@ -12,5 +19,60 @@ public class Mobile_Member {
 		this.daoM = daoM;
 	}
 	
-	
+	@RequestMapping("/Member_login.do")
+	public void find_member(HttpServletResponse response,@RequestParam("id") String id,@RequestParam("pwd") String pwd){
+		
+		Member member = daoM.getMember(id);
+		String blank = null;
+		if(member!=null){
+			if(member.getM_pwd().equals(pwd)){
+				JSONObject jsonObject = new JSONObject();
+				
+				jsonObject.put("result", 1);
+				jsonObject.put("member", member);
+				
+				System.out.println(member+"1");
+				try{
+					response.setContentType("application/json");
+					response.setCharacterEncoding("EUC-KR");
+					response.getWriter().write(jsonObject.toString());
+					
+					
+				}catch(Exception e){
+					e.printStackTrace();
+				}
+		
+			}else{
+				JSONObject jsonObject = new JSONObject();
+				jsonObject.put("result", 2);
+				System.out.println(member+"2");
+				try{
+					response.setContentType("application/json");
+					response.setCharacterEncoding("EUC-KR");
+					response.getWriter().write(jsonObject.toString());
+					
+				}catch(Exception e){
+					e.printStackTrace();
+				}
+		
+			}
+		}else{
+			JSONObject jsonObject = new JSONObject();
+			jsonObject.put("result", 3);
+			System.out.println(member+"3");
+			try{
+				response.setContentType("application/json");
+				response.setCharacterEncoding("EUC-KR");
+				response.getWriter().write(jsonObject.toString());
+				
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+		}
+		
+		
+		
+		
+		
+	}
 }
